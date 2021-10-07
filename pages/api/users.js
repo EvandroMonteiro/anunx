0259470855184813f26c98586c79c8616d54984e
Evandro Monteiro <goodvandro@gmail.com>
2021-10-07 11:07:13 +0000

    api post users + password crypto

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,6 @@
 import dbConnet from '../../src/utils/dbConnect'
+import { crypto } from '../../src/utils/password'
+import UsersModel from '../../src/models/users'
 
 const users = async (req, res) => {
   const { method } = req
@@ -7,6 +9,19 @@ const users = async (req, res) => {
     case 'GET':
       await dbConnet()
       res.status(200).json({ success: true })
+      break
+
+    case 'POST':
+      const { name, email, password } = req.body
+      await dbConnet()
+      const passwordCrypto = await crypto(password)
+      const user = new UsersModel({
+        name,
+        email,
+        password: passwordCrypto
+      })
+      user.save()
+      res.status(201).json({ sucess: true })
       break
 
     default:
